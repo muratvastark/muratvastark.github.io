abfaff5fd57cbec7f9431990356112e796c9a66d
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2023-04-16 01:23:48 +0300

    .

--- components/cursor/index.tsx
+++ components/cursor/index.tsx
@@ -1,20 +1,20 @@
-import { FC, useState, useEffect } from "react";
+import { FC, useState, useEffect, useCallback } from "react";
 
 const Cursor: FC = () => {
     const [coords, setCoords] = useState({ xCoords: 0, yCoords: 0 });
     const [scale, setScale] = useState<number>(1);
 
-    const onMouseMove = (e: MouseEvent) => {
+    const onMouseMove = useCallback((e: MouseEvent) => {
         setCoords({ xCoords: e.clientX, yCoords: e.clientY });
-    }
+    }, [setCoords]);
 
-    const onMouseUp = () => {
+    const onMouseUp = useCallback(() => {
         setScale(1);
-    }
+    }, [setScale]);
 
-    const onMouseDown = () => {
+    const onMouseDown = useCallback(() => {
         setScale(1.25)
-    }
+    }, [setCoords]);
 
     useEffect(() => {
         window.addEventListener("mousemove", (e) => onMouseMove(e));
